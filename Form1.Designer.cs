d649547cf606fe4c2391d22d916545e31bb2ef23
Aditya (Addy) Varanasi <avaranas@bechtel.com>
2020-08-28 15:38:34 -0400

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace CreateAzureStencilsFromSVG
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.txtDestination = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(181, 115);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(251, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Create Stencils";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 157);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(776, 281);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(325, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "This app creates Visio stencils using SVG images downloaded from:";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(343, 9);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(292, 13);
+            this.linkLabel1.TabIndex = 5;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "https://docs.microsoft.com/en-us/azure/architecture/icons/";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(166, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Input folder (downloaded images):";
+            // 
+            // txtSource
+            // 
+            this.txtSource.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSource.Location = new System.Drawing.Point(181, 49);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(607, 20);
+            this.txtSource.TabIndex = 7;
+            this.txtSource.Text = "C:\\Temp\\Azure_Public_Service_Icons";
+            // 
+            // txtDestination
+            // 
+            this.txtDestination.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtDestination.Location = new System.Drawing.Point(181, 75);
+            this.txtDestination.Name = "txtDestination";
+            this.txtDestination.Size = new System.Drawing.Size(607, 20);
+            this.txtDestination.TabIndex = 9;
+            this.txtDestination.Text = "C:\\Temp\\azure";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(154, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Output folder (created stencils):";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtDestination);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtSource);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Name = "Form1";
+            this.Text = "Convert Azure SVG files to Visio Stencils";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.TextBox txtDestination;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using MOIV = Microsoft.Office.Interop.Visio;
+
+namespace CreateAzureStencilsFromSVG
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MOIV.Application a = null;
+            string pf = txtDestination.Text;
+
+            try
+            {
+                // Validate
+                if (txtSource.Text.Length == 0)
+                    throw new Exception("Source was not specified");
+                else if (!Directory.Exists(txtSource.Text))
+                    throw new Exception("Source does not exist");
+                else if (new DirectoryInfo(txtSource.Text).GetDirectories().Length == 0)
+                    throw new Exception("Azure icons were not proreply downloaded.\nExpecting sub-folders in the source folder but didnt find any.");
+                else if (txtDestination.Text.Length == 0)
+                    throw new Exception("Destination was not specified");
+
+                // Prepare destination
+                if (!Directory.Exists(txtDestination.Text))
+                    Directory.CreateDirectory(txtDestination.Text);
+
+                textBox1.Text = "";
+                textBox1.Refresh();
+
+                // Open Visio
+                a = new MOIV.Application();
+
+                // Loop through the downloaded folders
+                foreach (string s in Directory.EnumerateDirectories(txtSource.Text))
+                {
+                    // Build stencil (file) name
+                    DirectoryInfo di = new DirectoryInfo(s);
+                    string sn = di.Name;
+                    if (!sn.StartsWith("azure", StringComparison.CurrentCultureIgnoreCase))
+                        sn = "Azure - " + sn;
+                    else
+                        sn = sn.Replace("Azure", "Azure -");
+                    textBox1.Text += sn + Environment.NewLine;
+                    textBox1.Refresh();
+
+                    // Create the new stencil and add svg files
+                    MOIV.Document d = a.Documents.Add(Application.StartupPath + "\\Template.vssx");
+                    foreach (FileInfo fi in di.GetFiles("*.svg"))
+                    {
+                        string sf = fi.Name;
+                        int i = sf.IndexOf("-service-", StringComparison.CurrentCultureIgnoreCase);
+                        sf = sf.Substring(i + "-service-".Length);
+                        sf = sf.Replace(".svg", "");
+                        sf = sf.Replace("-", " ");
+
+                        MOIV.Master m = d.Masters.Add();
+                        m.Name = sf;
+                        m.Import(fi.FullName);
+
+                        textBox1.Text += "     " + sf + Environment.NewLine;
+                        textBox1.Refresh();
+
+                        Application.DoEvents();
+                        Application.DoEvents();
+                    }
+
+                    // Save and close the stencil
+                    d.SaveAs(pf + "\\" + sn + ".vssx");
+                    d.Close();
+                    d = null;
+                    textBox1.Text += "---- Created";
+                    textBox1.Refresh();
+
+                    textBox1.Text += Environment.NewLine;
+                    textBox1.Refresh();
+                    Application.DoEvents();
+                    Application.DoEvents();
+                }
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                if (a != null)
+                {
+                    a.Quit();
+                    a = null;
+                }
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            linkLabel1.LinkVisited = true;
+            System.Diagnostics.Process.Start(linkLabel1.Text);
+        }
+    }
+}
